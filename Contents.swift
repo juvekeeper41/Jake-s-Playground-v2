7057b96472ee358bac6508546bd860397c12a77f
juvekeeper41 <juvekeeper41@gmail.com>
2016-09-15 09:27:11 -0600

    Created program and completed two string methods

---
+++ Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let jake = "👌🏾👶🏿"
+jake
+jake
+let jakeIsAwesome = true
+let jakeIsFunny = false
+jakeIsAwesome
+jakeIsFunny
+if jakeIsAwesome {
+    print("Jake is awesome!")
+} else {
+    print("Jake is not awesome!")
+}
+jakeIsAwesome
+var surveyAnswer: String?
+var serverResponseCode: Int? = 404
+serverResponseCode
+print("I am a smart program.")
